408664cf69b082c83e1a94f727bd308785612122
TruongTho <thonguyen@rtanalytics.vn>
2024-05-01 02:45:39 +0700

    update: framework

---
+++ Pods/Target Support Files/ValidEmailFrameWork/ValidEmailFrameWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ValidEmailFrameWork : NSObject
+@end
+@implementation PodsDummy_ValidEmailFrameWork
+@end
--- UsingFrameWorkRemote/ViewController.swift
+++ UsingFrameWorkRemote/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
-
+import ValidEmailFrameWork
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		Validator.sayHello()
+		let email1: String = "tho"
+		let email2: String = "tho@gmail.com"
+		print("Result \(email1): \(Validator.validEmail(email1))")
+		print("Result \(email2): \(Validator.validEmail(email2))")
 	}
 
 
