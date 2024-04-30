81e64a6d39d6a01ec3e0a8a9a3046bf341a1dcba
TruongTho <thonguyen@rtanalytics.vn>
2024-05-01 02:18:59 +0700

    Initial Commit

---
+++ UsingFrameWorkRemote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsingFrameWorkRemote
+//
+//  Created by rta on 5/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
